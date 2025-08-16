05d8ad949412c27c629bf92e74f31b480552e5a3
Santiago Peña Nieto <santip2357@gmail.com>
2025-08-16 09:54:07 -0500

    initial structure (bakend and frontend)

---
+++ backend/src/test/java/io/github/Lordrap2002/inventory/InventorySalesSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Lordrap2002.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorySalesSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
